83d42de341c2604dc8178b995c10d30a0b5ec66b
paulius.valintelis <paulius@insoft.lt>
2018-04-10 13:21:04 -0400

    Stream url function progress

--- media-functions-for-logic-app/save-video-stream-url/index.js
+++ media-functions-for-logic-app/save-video-stream-url/index.js
@@ -1,7 +1,23 @@
+var Client = require('node-rest-client').Client;
+
 module.exports = function (context, req) {
   context.log('This function saves video stream url');
-  
-  //const formValues = qs.parse(req.body);
+
+  var client = new Client();
+
+  var args = {
+    data: {
+      fileName: req.body.fileName,
+      streamUrl: req.body.streamUrl
+    },
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  client.post("https://development-api.spinfireapp.com/stream/streamUrl", args, function (data, response) {
+    context.log(response);
+  });
 
   context.res = {
     status: 200,
@@ -9,6 +25,6 @@ module.exports = function (context, req) {
     headers: { 'Content-Type': 'application/xml' },
     isRaw: true
   };
-  
+
   context.done();
 };
